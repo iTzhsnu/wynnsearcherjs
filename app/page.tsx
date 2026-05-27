de22c9a40fd503e3ab4c92d917dd80305c2cc07b
iTzhsnu <shunoosphere@outlook.jp>
2026-05-27 21:38:01 +0900

    change page.tsx and config

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import nextConfig from "../next.config";
+ const BASE_PATH = nextConfig.basePath || "";
 
 export default function Home() {
   return (
@@ -6,7 +8,7 @@ export default function Home() {
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
+          src="${BASE_PATH}/next.svg"
           alt="Next.js logo"
           width={100}
           height={20}
@@ -43,7 +45,7 @@ export default function Home() {
           >
             <Image
               className="dark:invert"
-              src="/vercel.svg"
+              src="${BASE_PATH}/vercel.svg"
               alt="Vercel logomark"
               width={16}
               height={16}
