4932c2b356efbc739d1dc0775c611533109be02d
iTzhsnu <shunoosphere@outlook.jp>
2026-05-27 23:13:58 +0900

    add main ui (first time)

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,126 @@
-import Image from "next/image";
+import styles from "./styles.module.css";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className={styles.main}>
+            <div className="filter-area">
+                <input className={styles.search_input} type="text" id="name-filter-field" placeholder="Name Filter"/>
+                <button className={styles.search_button} id="search-button">Search</button>
+                
+                <div className={styles.inline_block}>
+                    <div>
+                        <select className={styles.item_type} id="item-type-select">
+                            <option value="equipmets">Type: Equipmets</option>
+                            <option value="ingredients">Type: Ingredients</option>
+                            <option value="others">Type: Others</option>
+                            <option value="aspects">Type: Aspects</option>
+                        </select>
+                    </div>
+
+                    <div>
+                        <select className={styles.item_type} id="rarity-type-select">
+                            <option value="any">Rarity: Any</option>
+                            <option value="no-normal">Rarity: No Normal</option>
+                            <option value="mythic">Rarity: Mythic</option>
+                            <option value="fabled">Rarity: Fabled</option>
+                            <option value="legendary">Rarity: Legendary</option>
+                            <option value="rare">Rarity: Rare</option>
+                            <option value="unique">Rarity: Unique</option>
+                            <option value="normal">Rarity: Normal</option>
+                        </select>
+                    </div>
+                </div>
+                
+
+                <div className={styles.inline_block}>
+                    <label><input className={styles.type_checkbox} type="checkbox" id="bow-checkbox" />Bow</label>
+                    <label><input className={styles.type_checkbox} type="checkbox" id="spear-checkbox" />Spear</label>
+                    <label><input className={styles.type_checkbox} type="checkbox" id="wand-checkbox" />Wand</label>
+                    <label><input className={styles.type_checkbox} type="checkbox" id="dagger-checkbox" />Dagger</label>
+                    <label><input className={styles.type_checkbox} type="checkbox" id="relik-checkbox" />Relik</label>
+                    
+                    <label><input className={styles.type_checkbox} type="checkbox" id="ring-checkbox" />Ring</label>
+                    <label><input className={styles.type_checkbox} type="checkbox" id="bracelet-checkbox" />Bracelet</label>
+
+                    <div>
+                        <label><input className={styles.type_checkbox} type="checkbox" id="helmet-checkbox" />Helmet</label>
+                        <label><input className={styles.type_checkbox} type="checkbox" id="chestplate-checkbox" />Chestplate</label>
+                        <label><input className={styles.type_checkbox} type="checkbox" id="leggings-checkbox" />Leggings</label>
+                        <label><input className={styles.type_checkbox} type="checkbox" id="boots-checkbox" />Boots</label>
+                        <label><input className={styles.type_checkbox} type="checkbox" id="necklace-checkbox" />Necklace</label>
+                    </div>
+                </div>
+
+                <div>
+                    Sort:  
+                    <input id="id-box-11" list="ids" className={styles.ids_box} />
+                    +
+                    <input id="id-box-12" list="ids" className={styles.ids_box} />
+                    +
+                    <input id="id-box-13" list="ids" className={styles.ids_box} />
+                    +
+                    <input id="id-box-14" list="ids" className={styles.ids_box} />
+                </div>
+                <div>
+                  <input id="id-range-min-1" className={styles.ids_range} />
+                  to
+                  <input id="id-range-max-1" className={styles.ids_range} />
+                </div>
+
+                <div>
+                    Filter 1:
+                    <input id="id-box-21" list="ids" className={styles.ids_box} />
+                    +
+                    <input id="id-box-22" list="ids" className={styles.ids_box} />
+                    +
+                    <input id="id-box-23" list="ids" className={styles.ids_box} />
+                    +
+                    <input id="id-box-24" list="ids" className={styles.ids_box} />
+                </div>
+                <div>
+                  <input id="id-range-min-2" className={styles.ids_range} />
+                  to
+                  <input id="id-range-max-2" className={styles.ids_range} />
+                </div>
+
+                <div>
+                    Filter 2:
+                    <input id="id-box-31" list="ids" className={styles.ids_box} />
+                    +
+                    <input id="id-box-32" list="ids" className={styles.ids_box} />
+                    +
+                    <input id="id-box-33" list="ids" className={styles.ids_box} />
+                    +
+                    <input id="id-box-34" list="ids" className={styles.ids_box} />
+                </div>
+                <div>
+                  <input id="id-range-min-3" className={styles.ids_range} />
+                  to
+                  <input id="id-range-max-3" className={styles.ids_range} />
+                </div>
+
+                <div>
+                    Filter 3:
+                    <input id="id-box-41" list="ids" className={styles.ids_box} />
+                    +
+                    <input id="id-box-42" list="ids" className={styles.ids_box} />
+                    +
+                    <input id="id-box-43" list="ids" className={styles.ids_box} />
+                    +
+                    <input id="id-box-44" list="ids" className={styles.ids_box} />
+                </div>
+                <div>
+                  <input id="id-range-min-4" className={styles.ids_range} />
+                  to
+                  <input id="id-range-max-4" className={styles.ids_range} />
+                </div>
+
+                <datalist id="ids">
+                    <option value="Level" id="Level"></option>
+                    <option value="Strength Req" id="Strength Req"></option>
+                </datalist>
+
+            </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
   );
 }
