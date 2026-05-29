93f95324d2ab37b16069c7620abfa1bcc0787f18
iTzhsnu <shunoosphere@outlook.jp>
2026-05-29 09:39:20 +0900

    configをtsからmjsにして、ymlを調整

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: "/wynnsearcherjs",
+  assetPrefix: "/wynnsearcherjs/",
+};
+
+export default nextConfig;
